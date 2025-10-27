5137ead5651806f02de62e84cffcca29aa643117
Pedro Ramos Paraiso <125667163+Pedroca011@users.noreply.github.com>
2025-10-27 15:41:24 -0300

    Delete estudos/site-blog directory

--- estudos/site-blog/src/components/avatar/avatar-content.tsx
+++
@@ -1,7 +0,0 @@
-type AvatarContentProps = {
-  children: React.ReactNode;
-};
-
-export const AvatarContent = ({ children }: AvatarContentProps) => {
-  return <div className="flex flex-col">{children}</div>;
-};
